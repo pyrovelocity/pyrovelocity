a6aa964bae07e2960097e109138b3156ea604e8e
Cameron Smith <cameron.ray.smith@gmail.com>
2025-07-18 11:26:01 -0400

    fix(train): skip long-running doctest

--- src/pyrovelocity/tasks/train.py
+++ src/pyrovelocity/tasks/train.py
@@ -365,6 +365,7 @@ def train_model(
         Tuple[AnnData, PyroVelocity, Dict[str, ndarray]]: A tuple containing the processed AnnData object, the trained PyroVelocity model, and a dictionary of posterior samples.
 
     Examples:
+        >>> # xdoctest: +SKIP
         >>> from pyrovelocity.tasks.train import train_model
         >>> from pyrovelocity.utils import generate_sample_data
         >>> from pyrovelocity.tasks.preprocess import copy_raw_counts
